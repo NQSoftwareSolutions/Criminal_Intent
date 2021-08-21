16e0984cd2ba8ca7cd419d7ae4ba4220ed50b77c
AbdulQayoomJat <abdulqayoomjat9@gmail.com>
2021-08-21 09:52:33 +0500

    Fragment adding is done & setup completed

---
+++ app/src/main/java/com/nqsoftwaresolutions/criminalintent/CrimeFragment.java
@@ -0,0 +1,73 @@
+package com.nqsoftwaresolutions.criminalintent;
+
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class CrimeFragment extends Fragment {
+    private Crime mCrime;
+
+    /**
+     * @param savedInstanceState state of fragment
+     *                           called when fragment will be created
+     */
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mCrime = new Crime();
+    }
+
+    /**
+     * @param inflater Inflater by which we inflate our fragment on Activity
+     * @param container Container where our fragment will be
+     * @param savedInstanceState state of fragment
+     * @return view of fragment
+     */
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_crime,container,false);
+        wireWidgets(view);
+        return view;
+    }
+
+    private void wireWidgets(View view) {
+        Button mCrimeDateBtn = view.findViewById(R.id.id_btn_crimeDate);
+        EditText crimeTitleEt = view.findViewById(R.id.id_et_crimeTitle);
+        CheckBox isCrimeSolvedCb = view.findViewById(R.id.id_cb_crimeSolved);
+
+        crimeTitleEt.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        mCrimeDateBtn.setText(mCrime.getDate().toString());
+        mCrimeDateBtn.setEnabled(false);
+
+        isCrimeSolvedCb.setOnCheckedChangeListener((buttonView, isChecked) -> mCrime.setSolved(isChecked));
+    }
+}
--- app/src/main/java/com/nqsoftwaresolutions/criminalintent/MainActivity.java
+++ app/src/main/java/com/nqsoftwaresolutions/criminalintent/MainActivity.java
@@ -1,6 +1,8 @@
 package com.nqsoftwaresolutions.criminalintent;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
 
@@ -10,5 +12,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        configFragmentManager();
+    }
+
+    /**Todo Config Fragments
+     * Get Fragment Manager
+     * get fragment from fragment manager with id provided in parent activity layout
+     * check if fragment is null then create one & add
+     * fragment will not null when Activity is recreated
+     *      b/c of rotation of any other configuration change.
+     */
+    private void configFragmentManager() {
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.id_fl_crimeFragment);
+        addFragment(fm,fragment);
+    }
+
+    /**Todo Add a fragment
+     * @param fm fragment manager
+     * @param fragment fragment available in fragment manager
+     */
+    private void addFragment(FragmentManager fm, Fragment fragment) {
+        if (fragment == null){
+            fragment = new CrimeFragment();
+            fm.beginTransaction().add(R.id.id_fl_crimeFragment, fragment).commit();
+        }
     }
 }
